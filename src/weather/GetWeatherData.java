8cae8299f8e8a6659f4666f664d884d13e93c822
marekprog <marek.pierzchala.programista@gmail.com>
2019-10-18 19:09:52 +0200

    first commit

---
+++ src/weather/GetWeatherData.java
@@ -0,0 +1,21 @@
+package weather;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+//import com.google.gson.*;
+//import com.google.gson.reflect.*;
+
+/**
+ * Created by marek an 18.10.2019.
+ */
+public class GetWeatherData {
+    String API_KEY = "1948f4846ad801580c470dc03af314bc";
+
+
+}
